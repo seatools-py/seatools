2dce883af1d6f88a64ac4915001111f74da2f9c9
dragons96 <jinqunlong@black-unique.com>
2024-11-06 10:29:20 +0800

    feat: 优化undetected-chromedriver拓展, 新增API调用方法

--- seatools/uc/uc_chrome.py
+++ seatools/uc/uc_chrome.py
@@ -1,11 +1,13 @@
 import random
 import time
-from typing import Union, Callable, Dict, List
+from typing import Union, Callable, Dict, List, Literal, Optional, Any
 
+import requests
 import undetected_chromedriver as uc
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+from urllib.parse import urlparse
 
 
 class Chrome(uc.Chrome):
@@ -77,7 +79,7 @@ class Chrome(uc.Chrome):
         return self.find_elements(by, locator)
 
     @classmethod
-    def send_keys(cls, el: uc.WebElement, content: str, interval: Union[float, Callable[[], float]]=random.random):
+    def send_keys(cls, el: uc.WebElement, content: str, interval: Union[float, Callable[[], float]] = random.random):
         """给元素el输入内容, 每输入一个字符等待interval的间隔, 更拟人化
 
         Args:
@@ -108,3 +110,64 @@ class Chrome(uc.Chrome):
     def get_cookies_str(self) -> str:
         """获取拼接字符串的cookie, 示例: x=1; y=2"""
         return '; '.join([f'{k}={v}' for k, v in self.get_cookies_map().items()])
+
+    def invoke(self,
+               url,
+               method: Literal['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'CONNECT', 'TRACE'],
+               params: Dict[str, Any] = None,
+               data: Any = None,
+               json: Dict[str, Any] = None,
+               files: Any = None,
+               auth: Any = None,
+               timeout=None,
+               proxies=None,
+               stream=None,
+               verify=None,
+               cert=None,
+               headers: Optional[Dict[str, str]] = None):
+        """以http方式直接调用特定接口, 自动填充headers中不存在的ua, cookie等信息
+
+        Args:
+            url: 调用的链接
+            method: 调用的方法
+            params: url参数
+            data: 传递的数据
+            json: Content-Type 为 application/json 传递的数据
+            files: 传递的文件数据
+            auth: 认证信息
+            headers: 覆盖的请求头, 不存在的键将使用当前环境覆盖, 除非特别声明值为None
+            timeout: 超时时间, 默认不超时
+            proxies: 代理对象
+            stream: 是否以流方式处理响应
+            verify: 是否TLS校验
+            cert: TLS cert证书
+        """
+        headers = {k.lower(): v for k, v in headers.items()} if headers else {}
+        if 'user-agent' not in headers:
+            headers['user-agent'] = self.execute_script('return navigator.userAgent')
+        if 'cookie' not in headers:
+            headers['cookie'] = self.get_cookies_str()
+        if 'sec-ch-ua' not in headers:
+            brands = self.execute_script('return navigator.userAgentData.brands')
+            if brands:
+                sec_ch_uas = []
+                for brand in brands:
+                    if 'brand' not in brand or 'version' not in brand:
+                        continue
+                    sec_ch_uas.append(f'"{brand["brand"]}";v="{brand["version"]}"')
+                if sec_ch_uas:
+                    headers['sec-ch-ua'] = ', '.join(sec_ch_uas)
+        if 'sec-ch-ua-mobile' not in headers:
+            is_mobile = self.execute_script('return navigator.userAgentData.mobile')
+            headers['sec-ch-ua-mobile'] = f'?{"1" if is_mobile else "0"}'
+        if 'sec-ch-ua-platform' not in headers:
+            platform = self.execute_script('return navigator.userAgentData.platform')
+            if platform:
+                headers['sec-ch-ua-platform'] = f'"{platform}"'
+        if 'referer' not in headers:
+            headers['referer'] = self.current_url
+        if 'origin' not in headers:
+            parsed_url = urlparse(url)
+            headers['origin'] = f'{parsed_url.scheme}://{parsed_url.hostname}'
+        return requests.request(method, url, params=params, data=data, json=json, headers=headers, files=files,
+                                auth=auth, stream=stream, timeout=timeout, proxies=proxies, verify=verify, cert=cert)
