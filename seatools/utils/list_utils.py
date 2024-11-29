a139fffa46b91f0f774e3768e55c712bd361bc83
dragons96 <jinqunlong@black-unique.com>
2024-11-29 16:53:54 +0800

    fix

--- seatools/utils/list_utils.py
+++ seatools/utils/list_utils.py
@@ -1,6 +1,6 @@
 from typing import List, Tuple, Union, Any
 
 
-def get(l: Union[List[Any, ...], Tuple[Any, ...]], index: int, _default=None) -> Any:
+def get(l: Union[List[Any], Tuple[Any, ...]], index: int, _default=None) -> Any:
     return l[index] if len(l) > index else _default
 
