68950262277d49ba9afae8e8a71d2b6028733d68
dragons96 <jinqunlong@black-unique.com>
2024-12-04 10:34:09 +0800

    release: v1.0.16

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitee.com/dragons96/seatools',
-    version='1.0.15',
+    version='1.0.16',
     zip_safe=False,
 )
