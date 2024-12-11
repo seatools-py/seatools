4c6212272f6f355a447aee9d48a72eae1c25f1ef
dragons96 <jinqunlong@black-unique.com>
2024-12-11 14:41:51 +0800

    update

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitee.com/dragons96/seatools',
-    version='1.0.22',
+    version='1.0.23',
     zip_safe=False,
 )
