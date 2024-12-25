dd2ec25c451fb51e209ed9a8c19a189b7b27ba17
dragons96 <jinqunlong@black-unique.com>
2024-12-25 19:25:11 +0800

    release: 1.0.30

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitee.com/dragons96/seatools',
-    version='1.0.29',
+    version='1.0.30',
     zip_safe=False,
 )
