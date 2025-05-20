d6563cf629b997ec82fb622287c1b92bc5e99962
dragons96 <521274311@qq.com>
2025-05-20 14:14:58 +0800

    feat: redis工具新增async redis支持

--- seatools/redis/dbconfig.py
+++ seatools/redis/dbconfig.py
@@ -6,5 +6,6 @@ from seatools.ioc.database import DatabaseConfig
 class RedisConfig(DatabaseConfig):
     """通用 DB 配置"""
     config: Optional[dict] = None
+    is_async: Optional[bool] = False
     driver: Optional[str] = 'redis'
 
--- seatools/redis/utils/db.py
+++ seatools/redis/utils/db.py
@@ -5,5 +5,6 @@ from seatools.redis.dbconfig import RedisConfig
 def new_redis(config: RedisConfig, extra_config: Optional[dict] = None):
     import redis
     url = config.render_to_string(hide_password=False)
-    client = redis.Redis.from_url(url, **(extra_config or {}))
-    return client
+    if config.is_async:
+        return redis.asyncio.Redis.from_url(url, **(extra_config or {}))
+    return redis.Redis.from_url(url, **(extra_config or {}))
