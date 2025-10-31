bfc59af5309b1b673d16a7ff90b998cabbfb3167
dragons96 <521274311@qq.com>
2025-10-31 17:31:11 +0800

    release: 1.0.46

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitee.com/dragons96/seatools',
-    version='1.0.45',
+    version='1.0.46',
     zip_safe=False,
 )
